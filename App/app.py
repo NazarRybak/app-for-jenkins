90d5281717ce6789c535cb5af3d970f996762440
nazar_rybak <nazarrybak007@gmail.com>
2023-03-27 12:11:50 +0300

    Added timeout for Jenkins

--- App/app.py
+++ App/app.py
@@ -1,10 +1,18 @@
 from flask import Flask 
+import signal
 
+def handler(signum, frame):
+    raise Exception("Times out!")
 
 app = Flask(__name__) #екземпляр Flask
 
-
+signal.signal(signal.SIGALRM, handler)
+signal.alarm(15)
 @app.route('/')  #декоратор маршруту для URL-адреси
 
 def hello_world():
 	return 'Hello, thats my little web-app!'
+try:
+    app.run(debug=True)
+except Exception as ex:
+    print("Error: ", ex)
